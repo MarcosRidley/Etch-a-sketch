5d78a98d602188c2327116e5ee7aa1a5d90bf6e7
MarcosRidley <marcosridley@hotmail.com>
2021-12-14 14:01:55 -0300

    add functionality to button and trail on mouseenter

--- sketchbook.js
+++ sketchbook.js
@@ -4,6 +4,8 @@ const gameSpace = document.querySelector(".game-content");
 startGame.addEventListener("click", createDivs);
 
 function createDivs() {
+  deleteDivs();
+  //   startGame.classList.add("hidden");
   const divNumber = prompt("Qual o lado do quadrado?");
   if (divNumber > 100 || divNumber <= 0) {
     alert("Por favor digite um número entre 1 e 100");
@@ -16,6 +18,9 @@ function createDivs() {
   for (let i = 0; i < divNumber * divNumber; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("testingColor");
+    newDiv.addEventListener("mouseenter", () => {
+      newDiv.classList.add("add");
+    });
     gameSpace.appendChild(newDiv);
   }
 }
@@ -27,4 +32,12 @@ function createCSSvar(quantosFRs) {
     numeroFRs += " 1fr";
   }
   return numeroFRs;
+}
+
+function deleteDivs() {
+  const activeDivs = document.querySelectorAll(".testingColor");
+
+  for (let i = 0; i < activeDivs.length; i++) {
+    activeDivs[i].remove();
+  }
 }
